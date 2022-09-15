1dcc79fb14bbcc07cf1e0a70b2dbb558709c6637
somnatha656 <somnatha656@gmail.com>
2022-09-15 14:48:14 +0100

    my first commit

---
+++ src/githubpractice/Practice1.java
@@ -0,0 +1,19 @@
+package githubpractice;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        String myName = "Jalpa Patel";
+
+        int houseNo = 91;
+        String houseName = "Grove Road";
+        String county = "Dunstable";
+        String postCode = "LU5 5PD";
+
+        System.out.println(myName);
+   System.out.println(houseNo);
+        System.out.println(houseName);
+        System.out.println(county);
+        System.out.println(postCode);
+
+    }
+}
